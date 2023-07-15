83425e5d85209eedaefe3782a03951697d99c75a
Isham Faizal <ishamf@users.noreply.github.com>
2023-07-15 18:43:49 +0800

    feat: add build settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,22 @@
 import sucrase from '@rollup/plugin-sucrase';
-import resolve from '@rollup/plugin-node-resolve';
+import resolvePlugin from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy'
+
+import { realpathSync } from 'fs';
+import { resolve,dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /**
  * @type {import('rollup').RollupOptions}
  */
 const baseConfig = {
-    external: ['electron', '@prisma/client'],
+    external: ['electron'],
 
     plugins: [
-        resolve({
+        resolvePlugin({
             extensions: ['.js', '.ts']
         }),
         sucrase({
@@ -27,7 +34,16 @@ export default [
             file: '.app/build/bundle.js',
             format: 'cjs'
         },
-        ...baseConfig
+        ...baseConfig,
+        plugins: [
+            ...baseConfig.plugins,
+            copy({
+                targets: [
+                    { src: 'prisma/schema.prisma', dest: '.app/build' },
+                    { src: resolve(realpathSync(resolve(__dirname, 'node_modules/@prisma/client')), '../../.prisma/client/lib*'), dest: '.app/build' },
+                ]
+            })
+        ]
     },
     {
 
