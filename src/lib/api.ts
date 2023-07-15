b3982e992ac05def71a597798764a2746c97c14e
Isham Faizal <ishamf@users.noreply.github.com>
2023-07-16 00:42:06 +0800

    feat: initial tauri

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,25 +4,20 @@ import type { DataUpdateEvent } from './types';
 export type AppAPIType = typeof API;
 
 export const appAPI = (depends?: (id: string) => void) =>
-  new Proxy(
-    {},
-    {
-      get: <P extends keyof AppAPIType>(target: unknown, prop: P) => {
-        return async (...args: Parameters<AppAPIType[P]>) => {
-          if (depends) {
-            const dependsStringFn = internalDependsString[prop];
-            if (dependsStringFn) depends(dependsStringFn(...args));
-          }
-
-          // console.debug('Calling API function', prop, args);
-          const prev = performance.now();
-          const result = await electronAPI.invokeProxiedFunction(prop, ...args);
-          console.debug('Called API function', prop, args, result, performance.now() - prev);
-          return result;
-        };
-      },
+  // @ts-expect-error testing only
+  ({
+    setWindowState() {
+      console.log('pass');
     },
-  ) as typeof API;
+    getCommands() {
+      return [];
+    },
+    getPlatformDetails() {
+      return {
+        pathSeparator: '/',
+      }
+    }
+  } as typeof API);
 
 export const apiUrls = {
   getCommands: () => 'electronAPI:getCommands',
@@ -33,8 +28,9 @@ const internalDependsString: {
   [k in keyof AppAPIType]?: (...args: Parameters<AppAPIType[k]>) => string;
 } = apiUrls;
 
-export const onDataUpdate = (callback: (data: DataUpdateEvent) => void) =>
-  electronAPI.onDataUpdate(callback);
+export const onDataUpdate = (callback: (data: DataUpdateEvent) => void) => () => {
+  console.log('pass');
+};
 
 export const onNewLogLines = (commandId: number, callback: () => void) =>
   onDataUpdate((data) => {
